207b4b0d5c08c07a40d198e33fac3118e863b708
pandasoli <sadpandasoli@gmail.com>
2023-04-15 21:53:10 -0300

    feat: add everything

---
+++ src/routes.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from 'pages/home'
+
+
+export default () =>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={ <Home/> }/>
+    </Routes>
+  </BrowserRouter>
